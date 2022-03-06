9c2c09ca6dcee74196eca7f6a42b2a9550a25428
AndreasSitumorang <andreassitumorang098@gamil.com>
2022-03-06 20:16:28 +0700

    split commponent

--- src/Component/ExpenseItem.js
+++ src/Component/ExpenseItem.js
@@ -1,17 +1,16 @@
-import './ExpenseItem.css'
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
 
-function ExpenseItem(){
-    const expenseDate = new Date(2020, 9, 5);
-    const expenseAmount = 10; 
-    const expenseDescription = 'Car Lamborgini';
-
-    return (<div className='expense-item'>
-        <div> {expenseDate.toISOString()}</div>
-        <div className='expense-item__description'>
-            <h2> {expenseDescription}</h2>
-            <div className='expense-item__price'> {expenseAmount} IDR </div>
-        </div>
-    </div>);
+function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2> {props.title}</h2>
+        <div className="expense-item__price"> {props.amount} IDR </div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
